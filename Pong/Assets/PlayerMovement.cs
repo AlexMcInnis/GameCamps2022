2a413a90833dc60e40f0b6b1a563e13de5d2cf3a
AlexMcInnis <74010229+AlexMcInnis@users.noreply.github.com>
2022-06-09 11:12:25 -0300

    PlayerMovement Tutorial

---
+++ Pong/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float yBound = 2.25f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = rb.velocity;
+        if(Input.GetAxis("Vertical") > 0f)
+        {
+            vel.y = speed;
+        }
+        else if(Input.GetAxis("Vertical") < 0f)
+        {
+            vel.y = -speed;
+        }
+        else
+        {
+            vel.y = 0;
+        }
+        rb.velocity = vel;
+
+        var pos = transform.position;
+        if(pos.y > yBound)
+        {
+            pos.y = yBound;
+        }
+        else if(pos.y < -yBound)
+        {
+           pos.y = -yBound;
+        }
+        transform.position = pos;
+    }
+}
