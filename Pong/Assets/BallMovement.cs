d6c8a035d645909a8d1025fcd48d4d613d4473bd
AlexMcInnis <74010229+AlexMcInnis@users.noreply.github.com>
2022-06-10 09:34:21 -0300

    Ball Movement

--- Pong/Assets/BallMovement.cs
+++ Pong/Assets/BallMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 200f;   
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        AddStartingForce();
+    }
+    private void AddStartingForce()
+    {
+        float x = Random.value < 0.5f ? -1f : 1f;
+        float y = Random.value < 0.5f ? 0f : 0f;
+        Vector2 dir = new Vector2(x, y);
+        rb.AddForce(dir * speed);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Player"))
+        {
+            Vector2 vel;
+            vel.x = rb.velocity.x;
+            vel.y = (rb.velocity.y / 2) + (coll.collider.attachedRigidbody.velocity.y / 3);
+            rb.velocity = vel;
+        }
     }
 }
