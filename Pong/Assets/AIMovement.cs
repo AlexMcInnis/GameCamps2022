50d201230feb9d5c7ef146c23c88ffd30466bd2a
AlexMcInnis <74010229+AlexMcInnis@users.noreply.github.com>
2022-06-10 10:22:31 -0300

    Updated AI Movement

---
+++ Pong/Assets/AIMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D ball;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       if(ball.velocity.x > 0f)
+        {
+            if(ball.position.y > this.transform.position.y)
+            {
+                rb.AddForce(Vector2.up * speed);
+            }
+            else if(ball.position.y < this.transform.position.y)
+            {
+                rb.AddForce(Vector2.down * speed);
+            }
+        }
+        else
+        {
+            if(transform.position.y > 0f)
+            {
+                rb.AddForce(Vector2.down * speed);
+            }else if(transform.position.y < 0f)
+            {
+                rb.AddForce(Vector2.up * speed);
+            }
+        }
+    }
+}
