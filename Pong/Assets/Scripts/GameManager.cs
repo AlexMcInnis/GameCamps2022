9b7a55879c6a62caf638198f82e961ab980ea736
AlexMcInnis <74010229+AlexMcInnis@users.noreply.github.com>
2022-06-14 13:06:45 -0300

    Keep Score

--- Pong/Assets/Scripts/GameManager.cs
+++ Pong/Assets/Scripts/GameManager.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public BallMovement ball;
     private int playerScore;
     private int computerScore;
-    
+    public TMP_Text player;
+    public TMP_Text computer;
+
     public void PlayerScores()
     {
         playerScore++;
-        Debug.Log(playerScore);
+        player.text = playerScore.ToString();
         this.ball.ResetPosition();
     }
 
     public void ComputerScores()
     {
         computerScore++;
-        Debug.Log(computerScore);
+        computer.text = computerScore.ToString();
         this.ball.ResetPosition();
     }
 }
